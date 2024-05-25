aa2a3a0bb26456d36c5202804a4d8bc3c3a53610
Oganesson897 <101081378+Oganesson897@users.noreply.github.com>
2024-05-25 17:03:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ print(f'Current working directory: {working_path}')
 
 cache_path = os.path.join(working_path, 'build', 'downloadCache')
 output_path = os.path.join(working_path, 'build', 'output')
-template_path = os.path.join(working_path, 'EnigTech2')
+template_path = os.path.join(working_path, 'artifacts')
 lwjgl_version = ''  # Stub value
 
 for folder in [cache_path, output_path, template_path]:
@@ -83,7 +83,7 @@ else:
 print('---> Prepare installer and template')
 
 Util.extractArchive(installer_pattern[0], installer_pattern[1], os.path.join(cache_path, 'installer'))
-shutil.copytree('EnigTech2', output_path, dirs_exist_ok=True, ignore=shutil.ignore_patterns('*net.minecraft.json'))
+shutil.copytree('artifacts', output_path, dirs_exist_ok=True, ignore=shutil.ignore_patterns('*net.minecraft.json'))
 
 # Read cleanroom version
 print('---> Reading Cleanroom version')
@@ -177,4 +177,4 @@ if COMMIT_HASH or RUN_JOB_URL:
 
 # Pack everything to a single archive
 print('---> Archiving instance')
-print('Saved in: ' + shutil.make_archive(os.path.join(working_path, 'build', 'CleanroomMMC'), 'zip', output_path))
+print('Saved in: ' + shutil.make_archive(os.path.join(working_path, 'build', 'EnigTech2'), 'zip', output_path))
